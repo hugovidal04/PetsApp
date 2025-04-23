c4ea30a6387939b5ecd329c4f93ba10149275afe
hugovidal_04 <hugovidalpol@gmail.com>
2025-04-23 21:28:49 +0200

    Crea una columna en la pantalla de inicio de sesión para alinear el contenido

--- app/src/main/java/com/example/petsapp/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/example/petsapp/presentation/login/LoginScreen.kt
@@ -1,8 +1,24 @@
 package com.example.petsapp.presentation.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.petsapp.ui.theme.Principal
 
 @Composable
 fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Principal)
+            .padding(28.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 }
