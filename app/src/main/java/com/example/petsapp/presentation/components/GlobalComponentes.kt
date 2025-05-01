591839e73f36f8b88285e1c0c47175bcb76c62b9
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-01 12:30:16 +0200

    Crea componente para una lista de imágenes y lo aplica en la pantalla de login

--- app/src/main/java/com/example/petsapp/presentation/components/GlobalComponentes.kt
+++ app/src/main/java/com/example/petsapp/presentation/components/GlobalComponentes.kt
@@ -1,14 +1,19 @@
 package com.example.petsapp.presentation.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,4 +37,20 @@ fun NormalText(
         ),
         textAlign = TextAlign.Center
     )
+}
+
+@Composable
+fun ListaImagenes(
+    imagenes: List<Painter>,
+    size: Dp = 200.dp
+) {
+    Row {
+        imagenes.forEach { imagen ->
+            Image(
+                painter = imagen,
+                contentDescription = null,
+                modifier = Modifier.size(size)
+            )
+        }
+    }
 }
