237349210a472a7905834a1b7f0d3522c670329f
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-01 12:30:16 +0200

    Crea componente para una lista de imágenes y lo aplica en la pantalla de login

--- app/src/main/java/com/example/petsapp/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/example/petsapp/presentation/login/LoginScreen.kt
@@ -2,14 +2,21 @@ package com.example.petsapp.presentation.login
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.petsapp.R
+import com.example.petsapp.presentation.components.ListaImagenes
 import com.example.petsapp.ui.theme.Principal
 
+@Preview
 @Composable
 fun LoginScreen() {
     Column(
@@ -19,6 +26,11 @@ fun LoginScreen() {
             .padding(28.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Spacer(modifier = Modifier.height(50.dp))
+        ListaImagenes(
+            listOf(
+                painterResource(R.drawable.huella),
+            ), size = 200.dp
+        )
     }
 }
