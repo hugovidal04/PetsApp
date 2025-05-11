b8ec8256b1b50cd41a2f372a3f175525ba9dc982
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-11 22:00:41 +0200

    Nueva pantalla y navegación para los administradores

--- app/src/main/java/com/example/petsapp/NavigationApp.kt
+++ app/src/main/java/com/example/petsapp/NavigationApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.petsapp.presentation.admin.AdminScreen
 import com.example.petsapp.presentation.home.HomeScreen
 import com.example.petsapp.presentation.initial.InitialScreen
 import com.example.petsapp.presentation.login.LoginScreen
@@ -21,6 +22,7 @@ fun NavigationApp(navHostController: NavHostController) {
         composable("login") {
             LoginScreen(
                 navigateToHome = { navHostController.navigate("home") },
+                navigateToAdmin = { navHostController.navigate("admin") },
                 navigateToSignup = { navHostController.navigate("signup") }
             )
         }
@@ -29,6 +31,9 @@ fun NavigationApp(navHostController: NavHostController) {
         }
         composable("home") {
             HomeScreen()
+        }
+        composable("admin") {
+            AdminScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/petsapp/presentation/admin/AdminScreen.kt
@@ -0,0 +1,8 @@
+package com.example.petsapp.presentation.admin
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AdminScreen() {
+
+}
