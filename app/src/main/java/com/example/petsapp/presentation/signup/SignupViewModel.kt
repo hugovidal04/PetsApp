093ef6d62d2d4a6f4c10db978c9ffd09c3bbc0bb
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-13 21:46:09 +0200

    Guarda los datos del registro en la base de datos

--- app/src/main/java/com/example/petsapp/presentation/signup/SignupViewModel.kt
+++ app/src/main/java/com/example/petsapp/presentation/signup/SignupViewModel.kt
@@ -2,16 +2,19 @@ package com.example.petsapp.presentation.signup
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignupViewModel : ViewModel() {
+
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
 
     fun signup(
         name: String,
         email: String,
         password: String,
         confirmPassword: String,
-        //termsAccepted: Boolean,
+        termsAccepted: Boolean,
         onSuccess: () -> Unit,
         onFailure: (String) -> Unit
     ) {
@@ -35,21 +38,34 @@ class SignupViewModel : ViewModel() {
                 onFailure("Las contraseñas no coinciden.")
                 return
             }
-            /*!termsAccepted -> {
+
+            !termsAccepted -> {
                 onFailure("Debes aceptar los términos y condiciones.")
                 return
-            }*/
+            }
         }
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    println("Registro exitoso")
-                    onSuccess()
+                    val userId = auth.currentUser?.uid
+                    val userMap = hashMapOf(
+                        "name" to name,
+                        "email" to email,
+                        "isAdmin" to false,
+                        "acceptedTerms" to true
+                    )
+                    userId?.let {
+                        db.collection("users").document(it).set(userMap)
+                            .addOnSuccessListener {
+                                onSuccess()
+                            }
+                            .addOnFailureListener { e ->
+                                onFailure("Error al guardar datos: ${e.message}")
+                            }
+                    } ?: onFailure("Error al obtener ID del usuario")
                 } else {
-                    val errorMessage = task.exception?.message ?: "Error desconocido"
-                    println("Error en registro: $errorMessage")
-                    onFailure(errorMessage)
+                    onFailure("Error al registrar: ${task.exception?.message}")
                 }
             }
     }
