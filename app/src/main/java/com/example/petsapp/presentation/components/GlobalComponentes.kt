59461cd29518d2b07acd56001492faca2fd1e8a5
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-19 21:05:32 +0200

    Cambia colores de la barra de búsqueda

--- app/src/main/java/com/example/petsapp/presentation/components/GlobalComponentes.kt
+++ app/src/main/java/com/example/petsapp/presentation/components/GlobalComponentes.kt
@@ -9,28 +9,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
@@ -38,7 +26,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -131,35 +118,6 @@ fun ButtonComponent(
         )
     ) {
         Text(text = text)
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview
-@Composable
-fun BarraBusqueda() {
-    var query by remember { mutableStateOf("") }
-    var active by remember { mutableStateOf(false) }
-
-    SearchBar(
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(max = 900.dp),
-        query = query,
-        onQueryChange = { query = it },
-        onSearch = { println("Búsqueda confirmada: $it") },
-        active = active,
-        onActiveChange = { active = it },
-        placeholder = { Text("Buscar por nombre o correo") },
-        leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Buscar") },
-        colors = SearchBarDefaults.colors(
-            containerColor = Color(0xFFf1e7e4),
-            inputFieldColors = TextFieldDefaults.colors(
-                focusedTextColor = Color.White
-            )
-        )
-    ) {
-
     }
 }
 
