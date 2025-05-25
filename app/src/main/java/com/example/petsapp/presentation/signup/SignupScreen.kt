c5ec04fbd496f61786346ea573add2aa58620443
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-25 15:37:54 +0200

    Mensajes de errror en la UI al crear usuarios

--- app/src/main/java/com/example/petsapp/presentation/signup/SignupScreen.kt
+++ app/src/main/java/com/example/petsapp/presentation/signup/SignupScreen.kt
@@ -23,9 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -51,7 +53,7 @@ fun SingupScreen(
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
     var termsAccepted by remember { mutableStateOf(false) }
-    var errorMessage by remember { mutableStateOf<String?>(null) }
+    var errorMessage by remember { mutableStateOf("") }
 
     Box(
         modifier = Modifier
@@ -144,16 +146,23 @@ fun SingupScreen(
                             fontSize = 16.sp
                         )
                     }
-                    /*errorMessage?.let {
-                        Text(text = it, color = Color.Red, fontSize = 14.sp)
-                    }*/
+                    if (errorMessage.isNotEmpty()) {
+                        Text(
+                            text = errorMessage,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                     ButtonComponent(
                         modifier = Modifier.fillMaxWidth(),
                         onClick = {
                             viewModel.signup(
                                 name, email, password, confirmPassword, termsAccepted,
                                 onSuccess = navigateToHome,
-                                onFailure = { msg -> errorMessage = msg }
+                                onFailure = { error ->
+                                    errorMessage = error
+                                }
                             )
                         },
                         text = stringResource(id = R.string.boton_crear_cuenta_user),
