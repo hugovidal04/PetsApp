32b99860adfbf70791e1ac6441289edd26bfa9d7
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-27 19:02:14 +0200

    Crea modelo para mascotas

---
+++ app/src/main/java/com/example/petsapp/model/Pet.kt
@@ -0,0 +1,10 @@
+package com.example.petsapp.model
+
+data class Pet(
+    val id: String = "",
+    val name: String = "",
+    val species: String = "",
+    val breed: String = "",
+    val birthDate: String = "",
+    val gender: String = ""
+)
