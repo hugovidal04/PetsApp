0e2974a0b98dc2c471f5a101ac21db141c310bf8
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-29 22:52:41 +0200

    Mover paquete de calendario

--- app/src/main/java/com/example/petsapp/presentation/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/petsapp/presentation/calendar/CalendarScreen.kt
@@ -1,4 +1,4 @@
-package com.example.petsapp.calendar
+package com.example.petsapp.presentation.calendar
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
