3267a4b2dd4ab741d15808fd810c0854bd690a94
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-30 21:28:51 +0200

    Modelo para la alimentación

---
+++ app/src/main/java/com/example/petsapp/model/Feeding.kt
@@ -0,0 +1,10 @@
+package com.example.petsapp.model
+
+data class Feeding(
+    val id: String = "",
+    val petName: String = "",
+    val date: String = "",
+    val time: String = "",
+    val food: String = "",
+    val notes: String = ""
+)
