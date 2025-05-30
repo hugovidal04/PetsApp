6877f4488529915c47e0a3b16f71ff0ecb86d6da
hugovidal_04 <hugovidalpol@gmail.com>
2025-05-30 23:51:26 +0200

    Modelo de diario

---
+++ app/src/main/java/com/example/petsapp/model/DiaryEntry.kt
@@ -0,0 +1,10 @@
+package com.example.petsapp.model
+
+data class DiaryEntry(
+    val id: String = "",
+    val petName: String = "",
+    val date: String = "",
+    val title: String = "",
+    val content: String = ""
+)
+
