efe7b4c20eab71df123c8c38ce65a1c390946c60
hugovidal_04 <hugovidalpol@gmail.com>
2025-06-02 22:48:58 +0200

    Cambia colores del botón de eliminar

--- app/src/main/java/com/example/petsapp/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/petsapp/presentation/settings/SettingsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -26,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -97,9 +97,9 @@ fun SettingsScreen(
 
             Button(
                 onClick = { showConfirmDialog = true },
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error)
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
             ) {
-                Text("Eliminar cuenta", color = MaterialTheme.colorScheme.onError)
+                Text("Eliminar cuenta", color = ColorTexto)
             }
         }
 
