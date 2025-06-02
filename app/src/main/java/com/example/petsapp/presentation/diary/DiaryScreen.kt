69bb21b2eebf8c14172d81b60523e678991cce9e
hugovidal_04 <hugovidalpol@gmail.com>
2025-06-02 21:18:36 +0200

    Añade grosor al título de diario

--- app/src/main/java/com/example/petsapp/presentation/diary/DiaryScreen.kt
+++ app/src/main/java/com/example/petsapp/presentation/diary/DiaryScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -53,7 +54,7 @@ fun DiaryScreen(
                 "Diario de la mascota",
                 color = ColorTexto,
                 fontSize = 22.sp,
-                modifier = Modifier.padding(bottom = 12.dp)
+                fontWeight = FontWeight.Bold
             )
 
             if (diaryEntries.isEmpty()) {
